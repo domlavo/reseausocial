3c0a278e39297723ae88ce51515316c45a1d6992
Lavoie Dominic <0839284@cegepthetford.ca>
2018-02-27 11:22:05 -0500

    bootstrap + css

--- header.php
+++ header.php
@@ -9,6 +9,9 @@ function renderHeader() {
 	<html lang="en">
 	<head>
 		<title>Your Website Title</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="/css/main.css">
 	</head>
 	<body>
 	<?php
