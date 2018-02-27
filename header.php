5062ba664f78cc321e08bba19a51933586e35558
Lavoie Dominic <0839284@cegepthetford.ca>
2018-02-27 11:30:53 -0500

    setup client

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ function renderHeader() {
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 		<meta name="google-signin-scope" content="profile email">
-		<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+		<meta name="google-signin-client_id" content="924453145091-qc766r3o6jboklodlm655e1v5tirmf43.apps.googleusercontent.com">
 		<script src="https://apis.google.com/js/platform.js" async defer></script>
 		<link rel="stylesheet" href="/css/main.css">
 	</head>
