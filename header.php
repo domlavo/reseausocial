88981a1f751de9bab58dce29577c28b2035d6c1e
Alexandre Dupont <aledupo@dinfo.cegepth.ca>
2018-02-27 11:28:30 -0500

    google sign in

--- header.php
+++ header.php
@@ -11,6 +11,9 @@ function renderHeader() {
 		<title>Your Website Title</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<meta name="google-signin-scope" content="profile email">
+		<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
 		<link rel="stylesheet" href="/css/main.css">
 	</head>
 	<body>
--- index.php
+++ index.php
@@ -5,9 +5,23 @@ require 'footer.php';
 echo renderHeader();
 ?>
 
-<div>
-  Hello
-</div>
+<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      };
+    </script>
 <?php
 echo renderFooter();
 ?>
